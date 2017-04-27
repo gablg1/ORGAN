3632f45afa8841abd92f3c918ad05063d2cfd390
Pedro Farias <PFarias@Pedros-MacBook-Pro.local>
2017-04-27 00:05:39 -0400

    Fixed music uploader

--- models/pedro/io_utils.py
+++ models/pedro/io_utils.py
@@ -16,19 +16,65 @@ def read_songs_txt(filename):
     with open(filename, 'rU') as file:
         data = []
         line = file.readline()
-        ignore_chars = {'T', '%', 'S', 'M', 'K', 'P'}
-        accumulator = ""
+        ignore_chars = {'X', 'T', '%', 'S', 'M', 'K', 'P', '\"', '\n', ' ', '(', ')', 'm', '-', '\\', '!', 't', 'r', 'i', 'l', 'z', '[', '+', 'n', 'o', '#'}
+        notes = {'C', 'D', 'E', 'F', 'G', 'A', 'B', 'c', 'd', 'e', 'f', 'g', 'a', 'b'}
+        prev_string = ""
+        same_note = False
+        on_bar = False
         while line != '':
             line = file.readline()
-            if not line:
+            if not line or line[0] in ignore_chars:
                 continue
-            if line[0] == 'X':
-                data.append(accumulator.strip('\n'))
-                accumulator = ""
-            if line[0] in ignore_chars:
-                continue
-            else:
-                accumulator += line
+            for c in line:
+                if c in ignore_chars:
+                    continue
+                elif c in notes:
+                    same_note = True
+                    length = len(prev_string)
+                    # previous string was a note
+                    if on_bar:
+                        on_bar = False
+                        data.append(prev_string)
+                        prev_string = c
+                    elif length != 0:
+                        first = prev_string[length - 1]
+                        if first in notes:
+                            data.append(prev_string)
+                            prev_string = c
+                        elif first == '/':
+                            prev_string = prev_string[1:]
+
+                    else:
+                        prev_string += c
+                # flats, sharps, and naturals - indicate a new note
+                elif c in {'_', '^', '='}:
+                    if not same_note:
+                        same_note = True
+                    data.append(prev_string)
+                    prev_string = c
+                # likely to indicate length
+                elif c.isdigit():
+                    # if in the same note, the num closes it up by indicating length - indicate end of note
+                    if same_note:
+                        data.append(prev_string + c)
+                        prev_string = ""
+                        same_note = False
+                # bars & repeats are separate from notes
+                elif c in {'|', ':'}:
+                    if same_note:
+                        data.append(prev_string)
+                        prev_string = ""
+                    if on_bar:
+                        data.append(prev_string + c) 
+                        prev_string = ""
+                        on_bar = False
+                    else:
+                        on_bar = True
+                        data.append(prev_string)
+                        prev_string = c
+                    same_note = False
+                else:
+                    prev_string += c
     return data
 
 def read_smiles_smi(filename):
